e1701d72a6c9eab4dae9cdf1ea6ffba0ff6aae93
PDuchnik <287378@student.pwr.edu.pl>
2025-03-24 18:22:14 +0100

    Update 287378_z2b.cpp

--- l2/z2/287378_z2b.cpp
+++ l2/z2/287378_z2b.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 using namespace std;
 #include "math.h"
-#include <string>
-#include <vector>
+
 int main(){
     int length = 0;
     int x;
-    vector<int> numbers;
+    int* numbers;
     int buffer;
-    for(int i = -2; i < length; i++){
-        cin >> buffer;
-        if (i == -2){
-            x = buffer;
-        }
-        else if(i == -1){
-            length = buffer;
-        }
-        else{
-            numbers.push_back(buffer);
-        }
+    cin >> x >> length;
+    for(int i = 0; i < length; i++){
+        cin >> numbers[i];
     }
-    int up = length;
+    int up = length - 1;
     int down = 0;
-    int average = std::floor((up + down)/2);
     bool flag = false;
-    while(up != down){
-        if(numbers[average] < x){
-            down = average;
+    float average = 0;
+    while(up >= down){
+        average = std::floor((up + down)/2);
+        if(numbers[(int)average] < x){
+            down = average + 1;
         }
-        else if(x < numbers[average]){
-            up = average;
+        else if(numbers[(int)average] > x){
+            up = average -1;
         }
-        else{
+        if(numbers[(int)average] == x){
             flag = true;
             break;
         }
-        average = std::floor((up + down)/2);
     }
     if(flag){
         cout << "tak "<< average << endl;
