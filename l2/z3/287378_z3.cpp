899458ebaf60310516e37f0de1d8099d00e6fb00
PDuchnik <287378@student.pwr.edu.pl>
2025-03-24 18:24:51 +0100

    Update 287378_z3.cpp

--- l2/z3/287378_z3.cpp
+++ l2/z3/287378_z3.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 #include <vector>
 int main(){
-    vector<bool> message;
+    bool* message;
     for(int i = 0; i < 16; i++){
-        bool a;
-        cin >> a;
-        message.push_back(a);
+       cin >> message[i];
     }
     for(int i = 0; i < 8; i++){
         cout << (message[i]^message[i+8]) << ' ';
