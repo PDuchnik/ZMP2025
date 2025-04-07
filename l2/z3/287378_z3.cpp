26d2374a1d580ceefce9ea0c0fdc4999b6561e03
PDuchnik <287378@student.pwr.edu.pl>
2025-04-07 22:46:47 +0200

    Update 287378_z3.cpp

--- l2/z3/287378_z3.cpp
+++ l2/z3/287378_z3.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #include <vector>
 int main(){
     bool* message;
+    cout << "Podaj sekret (ciag 0/1 o dlugosci 8) oraz wiadomosc (drugi ciag 0/1 o dlugosci 8):" << endl;
     for(int i = 0; i < 16; i++){
        cin >> message[i];
     }
